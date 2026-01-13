2bd6611de0dae4c42cd627f892643ac7980a9a5f
labstraction <labstraction@protonmail.com>
2026-01-13 12:36:31 +0100

    chart-points

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import "./style.css";
 import { MeteoService } from "./meteo-service.js";
-import { Chart } from "chart.js";
+import { Chart, LineController, CategoryScale, LinearScale, PointElement, LineElement} from "chart.js";
 
 const service = new MeteoService();
 
@@ -8,12 +8,18 @@ service.getMeteoData().then(meteoData => displayMeteo(meteoData));
 
 function displayMeteo(meteoData) {
 
+    const temperaturepoints = getTemperaturePoints(meteoData);
+    const rainpoints = getRainPoints(meteoData);
+    const windpoints = getWindPoints(meteoData);
+
+    testChart("temperature-chart", temperaturepoints);
+    testChart("rain-chart", rainpoints);
+    testChart("wind-chart", windpoints);
+
     const container = document.getElementById('app');
     container.innerHTML = "";
 
     for (const data of meteoData) {
-
-        console.log(data);
 
         const card = document.createElement('div');
 
@@ -42,10 +48,29 @@ function displayMeteo(meteoData) {
 
 }
 
+function getTemperaturePoints(meteoData) {
+    return [];
+}
 
-function testChart() {
+function getRainPoints(meteoData) {
+    return [];
+}
 
-    const labels = Utils.months({ count: 7 });
+function getWindPoints(meteoData) {
+    return [];
+}
+
+function testChart(canvasId, dataPoints) {
+
+    console.log("data points", dataPoints);
+
+    //[{x:"2026-01-13T00:00", y:9},
+    //{x:"2026-01-13T01:00", y:8},
+    //{x:"2026-01-13T02:00", y:7},...]
+
+    Chart.register(LineController, CategoryScale, LinearScale, PointElement, LineElement);
+
+    const labels = ["marzo", "aprile", "maggio", "giugno", "luglio", "agosto","settembre"]
 
     const data = {
         labels: labels,
@@ -63,8 +88,9 @@ function testChart() {
         data: data,
     };
 
-    const canvas = document.getElementById('pippo')
+    const canvas = document.getElementById(canvasId)
 
     new Chart(canvas, config)
 
 }
+
